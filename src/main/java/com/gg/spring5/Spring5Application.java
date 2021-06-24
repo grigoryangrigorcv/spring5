22e88314380de33f0e363d5c70e01ba86b7f0cfe
Grigor Grigoryan <grigor.grigoryan@webbfontaine.com>
2021-06-24 12:36:44 +0400

    test2

--- src/main/java/com/gg/spring5/Spring5Application.java
+++ src/main/java/com/gg/spring5/Spring5Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5Application {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Spring5Application.class, args);
     }
